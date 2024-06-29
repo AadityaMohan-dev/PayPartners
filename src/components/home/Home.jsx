2c16e8b642f64a4c1306c899033d9aa79efe8c70
aaditya mohan <aadimohan51@gmail.com>
2024-06-29 17:47:28 +0530

    updated

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { CgAdd } from "react-icons/cg";
 function Home() {
   return (
     <>
     <div id="home-container" className='p-5'>
-        <div id="content" className='shadow-lg rounded-xl py-5'>
-        <div id="greeting" className='flex justify-between shadow-lg rounded-lg p-5 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'>
-            <h1 className='text-2xl  capitalize text-white'>Hi, Aaditya Mohan !!</h1>
-            <button className='rounded-lg shadow-xl  font-semibold text-white capitalize border border-white items-center p-2'>Add new Expense </button>
+        <div id="content" className='shadow-lg rounded-xl bg-white'>
+        <div id="greeting" className='flex justify-between shadow-lg rounded-lg p-5 bg-gradient-to-r from-gray-900 to-gray-600'>
+            <h1 className='text-2xl  capitalize text-white'><span className='text-blue-500'>Hi</span>, Aaditya Mohan !!</h1>
+            <button className='text-white flex gap-10 capitalize font-semibold text-lg'>Add New expense<CgAdd className='text-3xl'/> </button>
         </div> 
-        <div id="content" className='py-5 divide-y w-full h-full'>
+        <div id="content" className='px-5 divide-y w-full h-full'>
             <div id="total-expense" className='text-xl font-sans  capitalize py-5'>Total Expense</div>
             <div id="dbts" className='text-xl font-sans capitalize  py-5'>
                 Debts remaining
