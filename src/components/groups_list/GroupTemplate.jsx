b36007e320ff4c629f50c522d77a3ed7713ce98d
aaditya mohan <aadimohan51@gmail.com>
2024-07-20 19:44:15 +0530

    comment added

--- src/components/groups_list/GroupTemplate.jsx
+++ src/components/groups_list/GroupTemplate.jsx
@@ -12,11 +12,11 @@ function GroupTemplate() {
             </div>
             <h1 className='py-5 text-2xl capitalize'>transactions</h1>
             <div id="update-transactions" className='shadow-lg backdrop-blur-md bg-white rounded-lg p-10 flex flex-wrap gap-20 justify-center my-5'>
-              
+              {/* Add list here */}
             </div>
             <h1 className='py-5 text-2xl capitalize'>debts</h1>
             <div id="updated-debts" className='shadow-lg backdrop-blur-md bg-white rounded-lg p-10 flex flex-wrap gap-20 justify-center my-5'>
-              
+              {/* Add list here */}
             </div>
             <h1 className='py-5 text-2xl capitalize'>total spent</h1>
             <div id="total-expense" className='shadow-lg backdrop-blur-md bg-white rounded-lg p-10 flex flex-wrap gap-20 justify-center my-5'>
@@ -24,7 +24,7 @@ function GroupTemplate() {
             </div>
             <h1 className='py-5 text-2xl capitalize'>group activity</h1>
             <div id="group-activity" className='shadow-lg backdrop-blur-md bg-white rounded-lg p-10 flex flex-wrap gap-20 justify-center my-5'>
-              
+              {/* Add list here */}
             </div>
         </div>
     </div>
