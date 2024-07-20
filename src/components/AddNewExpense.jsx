2ad0fc9f0a87f53cc8d343c74174481acdc88a38
aaditya mohan <aadimohan51@gmail.com>
2024-07-20 20:04:08 +0530

    resolve error

--- src/components/AddNewExpense.jsx
+++ src/components/AddNewExpense.jsx
@@ -1,277 +1,103 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { initializeApp } from "firebase/app";
-// import { db } from "../config";
-import {uid} from "uid";
-import { getDatabase, ref, push, set, onValue } from "firebase/database";
-import { useEffect } from "react";
-// import firebaseConfig from "../config"; // Path to your firebaseConfig.js
-
-// Initialize Firebase app
-// const app = initializeApp(firebaseConfig);
-
-function AddNewExpense() {
-
-  const [firstfield , setFirstField] = useState("");
-  const[fetchdata, setFetchdata]= useState([]);
-
-  const handlechangefunction = (e) => {
-    setFirstField(e.target.value);
-  }
-
- const  constWrite = () => {
-    const uuid = uid();
-    set(ref(db, `/${uuid}`),{
-      firstfield,
-    })
-    setFirstField("");
-  }
-
-//  useEffect(()=>{
-//     onValue(ref(db), (snapshot)=>{
-//       const data = snapshot.val();
-//       if(data != null){
-//         Object.values(data).map((firstfield)=>{
-//           setFirstField(oldArray => [...oldArray, firstfield])
-//         })
-//       }
-//     })
-//  },[])
-  // const [inputValue1, setInputValue1] = useState("");
-  // const [inputValue2, setInputValue2] = useState("");
-  // const [inputValue3, setInputValue3] = useState("");
-  // const [inputValue4, setInputValue4] = useState("");
-
-  // const saveData = async () => {
-  //   const db = getDatabase(app);
-  //   const newDocRef = push(ref(db, "ExpenseName/WhoPaid/ForWhom/Date"));
-  //   try {
-  //     await set(newDocRef, {
-  //       ExpenseName: inputValue1,
-  //       WhoPaid: inputValue2,
-  //       ForWhom:inputValue3,
-  //       Date:inputValue4
-  //     });
-  //     console.log("Data added successfully!");
-  //     alert("Data added successfully!");
-  //   } catch (error) {
-  //     console.error("Error adding data:", error.message);
-  //     alert("Error adding data: " + error.message);
-  //   }
-  // };
-
-  return (
-    <div id="main-container" className="p-10">
-      <div id="card" className="shadow-lg ba bg-white rounded-lg w-full p-5">
-        <div id="row-1" className="flex justify-between">
-          <h1 className="text-2xl leading-none text-gray-900 dark:text-white pb-5">
-            Add An Expense
-          </h1>
-        </div>
-        <form className="max-w-full mx-auto grid justify-center items-center grid-cols-2 gap-5">
-          <div className="mb-5">
-            <label
-              htmlFor="expense-name"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-=======
-import React, { useContext } from "react";
+import React from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { UserContext } from "./context/UserContextProvider";
-
+ 
 function AddNewExpense() {
-  const { setIsExpenseWindowOpen } = useContext(UserContext);
-
   return (
-    <div id="main-container" className="p-10">
-      <div
-        id="card"
-        className="shadow-lg ba bg-white rounded-lg  w-full md:w-[720px] p-5"
-      >
-        <div id="row-1" className="flex justify-between">
-          <h1 className="text-2xl  leading-none text-gray-900 dark:text-white pb-5">
-            Add An Expense
-          </h1>
-          <div
-            id="close-btn"
-            className="text-3xl md:text-xl cursor-pointer"
-            onClick={() => setIsExpenseWindowOpen(false)}
-          >
-            <IoCloseSharp className="rounded-full bg-white" />
-          </div>
-        </div>
-
-        <form class="max-w-full mx-auto grid justify-center items-center grid-cols-2 gap-5">
-          <div class="mb-5">
-            <label
+<div id="main-container" className="p-10">
+<div id="card" className="shadow-lg ba bg-white rounded-lg w-full p-5">
+<div id="row-1" className="flex justify-between">
+<h1 className="text-2xl  leading-none text-gray-900 dark:text-white pb-5">
+          Add An Expense
+</h1>
+<div id="close-row" className='text-3xl md:text-5xl fixed w-full flex justify-end p-10 cursor-pointer' onClick={()=>setIsAuthClose(true)}><IoCloseSharp /></div>
+</div>
+<form class="max-w-full mx-auto grid justify-center items-center grid-cols-2 gap-5">
+<div class="mb-5">
+<label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
->>>>>>> e57fdc93ff630fb5a3e53185362d415f2c6ffb4c
-            >
+>
               Expense Name
-            </label>
-            <input
-<<<<<<< HEAD
-              value={firstfield}
-              onChange={handlechangefunction}
-              // onChange={(e) => setInputValue1(e.target.value)}
-              type="text"
-              id="expense-name"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Expense Name"
-              required
-            />
-          </div>
-          {/* <div className="mb-5">
-            <label
-              htmlFor="paid-by"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Who Paid
-            </label>
-            <input
-              value={inputValue2}
-              onChange={(e) => setInputValue2(e.target.value)}
-              type="text"
-              id="paid-by"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Who Paid"
-              required
-            />
-          </div>
-          
-          <div className="mb-5">
-            <label
-              htmlFor="paid-by"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-             For Whom
-            </label>
-            <input
-              value={inputValue3}
-              onChange={(e) => setInputValue3(e.target.value)}
-              type="text"
-              id="paid-by"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Who Paid"
-              required
-            />
-          </div>
-
-          <div className="mb-5">
-            <label
-              htmlFor="paid-by"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Date
-            </label>
-            <input
-              value={inputValue4}
-              onChange={(e) => setInputValue4(e.target.value)}
-              type="text"
-              id="paid-by"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Who Paid"
-              required
-            />
-          </div> */}
-
-          <div id="btns" className="flex gap-10">
-            <button
-              type="button"
-              onClick={constWrite}
-              className="border border-blue-700 h-11 rounded-lg w-40 font-semibold text-blue-700 capitalize hover:bg-blue-700 hover:text-white hover:shadow-lg"
-            >
-              Save
-              {fetchdata.map((firstfield)=>{
-                <h1>
-                  {firstfield.firstfield}</h1>
-
-              })}
-              
-            </button>
-          </div> 
-=======
+</label>
+<input
               type="text"
               id="base-input"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Expense"
               required
             />
-          </div>
-          <div class="mb-5">
-            <label
+</div>
+<div class="mb-5">
+<label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
+>
               Expense Name
-            </label>
-            <input
+</label>
+<input
               type="text"
               id="base-input"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Expense"
               required
             />
-          </div>
-          <div class="mb-5">
-            <label
+</div>
+<div class="mb-5">
+<label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
+>
               Expense Name
-            </label>
-            <input
+</label>
+<input
               type="text"
               id="base-input"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Expense"
               required
             />
-          </div>
-          <div class="mb-5">
-            <label
+</div>
+<div class="mb-5">
+<label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
+>
               Expense Name
-            </label>
-            <input
+</label>
+<input
               type="text"
               id="base-input"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Expense"
               required
             />
-          </div>
-          <div class="mb-5">
-            <label
+</div>
+<div class="mb-5">
+<label
               for="base-input"
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
+>
               Expense Name
-            </label>
-            <input
+</label>
+<input
               type="text"
               id="base-input"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Expense"
               required
             />
-          </div>
-          <div id="btns" className="flex gap-10">
-            {/* delete button */}
-            {/* <button className="border border-red-700 h-12 rounded-lg w-40 font-semibold text-red-700 capitalize hover:bg-red-700 hover:text-white hover:shadow-lg">Delete</button>
-             */}
-            {/* save button */}
-            <button className="border border-blue-700 h-11 rounded-lg w-40 font-semibold text-blue-700 capitalize hover:bg-blue-700 hover:text-white hover:shadow-lg">
-              save
-            </button>
-          </div>
->>>>>>> e57fdc93ff630fb5a3e53185362d415f2c6ffb4c
-        </form>
-      </div>
-    </div>
+</div>
+<div id="btns" className="flex gap-10">
+              {/* delete button */}
+          {/* <button className="border border-red-700 h-12 rounded-lg w-40 font-semibold text-red-700 capitalize hover:bg-red-700 hover:text-white hover:shadow-lg">Delete</button>
+           */}
+          {/* save button */}
+<button className="border border-blue-700 h-11 rounded-lg w-40 font-semibold text-blue-700 capitalize hover:bg-blue-700 hover:text-white hover:shadow-lg">save</button>
+</div>
+</form>
+</div>
+</div>
   );
 }
-
+ 
 export default AddNewExpense;
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,44 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import React from 'react'
-=======
-import React, { useContext } from 'react'
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { useState } from 'react';
-import {Link} from 'react-router-dom';
-// import { auth } from '../../config/FirebaseConfig';
-import { auth } from '../../config';
-import { IoCloseSharp } from "react-icons/io5";
-import { UserContext } from '../context/UserContextProvider';
-function SignUp() {
-
-    const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-
-
-const signup = async () => {
-    if(email === "" || password === ""){
-        return alert("Please fill all fields")
-    }
-    try {
-        const user = await createUserWithEmailAndPassword(auth, email, password);
-        alert("Signup Successful");
-        setEmail("");
-        setPassword("");
-    } catch (error) {
-        console.log(error);
-    }
-}
-    const {setIsAuthClose} = useContext(UserContext);
->>>>>>> Stashed changes
-=======
 import React, { useContext, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import axios from "axios";
 import { UserContext } from "../context/UserContextProvider";
 import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
->>>>>>> e57fdc93ff630fb5a3e53185362d415f2c6ffb4c
 
 function SignUp() {
   const { setIsAuthClose } = useContext(UserContext);
@@ -106,74 +71,6 @@ function SignUp() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create an Account
               </h1>
-<<<<<<< HEAD
-              <form class="space-y-4 md:space-y-6" action="#">
-                  {/* <div>
-                      <label  name='email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)} for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                  </div> */}
-                  <div>
-  <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-  <input
-    type="email"
-    id="email"
-    name="email"
-    value={email} 
-    onChange={(e) => setEmail(e.target.value)}
-    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    placeholder="name@company.com"
-  />
-</div>
-
-<div>
-  <label htmlFor="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-  <input
-    id="password"
-    type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    placeholder="name@company.com"
-  />
-</div>
-
-                  {/* <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div>
-                      <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                      <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
-                  </div>
-                  <div class="flex items-start">
-                      <div class="flex items-center h-5">
-                        <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                      </div>
-                      <div class="ml-3 text-sm">
-                        <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                      </div>
-<<<<<<< Updated upstream
-                  </div>
-                  <button type="submit" class="w-full text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
-                  </p>
-=======
-                  </div> */}
-                  {/* <button type="submit" class="w-full text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button> */}
-                  <button
-   onClick={signup}
-   className=' bg-red-500 w-full text-white font-bold  px-2 py-2 rounded-lg'>
-    Sign up
-</button>
-                  {/* <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <a href="#" class="font-medium text-blue-900 hover:underline dark:text-primary-500">Login here</a>
-                  </p> */}
->>>>>>> Stashed changes
-              </form>
-=======
               <div
                 id="close-btn"
                 className="text-3xl md:text-xl  cursor-pointer"
@@ -271,7 +168,6 @@ function SignUp() {
                 </Link>
               </p>
             </form>
->>>>>>> e57fdc93ff630fb5a3e53185362d415f2c6ffb4c
           </div>
         </div>
       </div>
